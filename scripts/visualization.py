9c9dd798c44918ec19889f9c9eaa44f9e06ae771
Ravi Joshi <ravi2008joshi@gmail.com>
2018-11-27 15:34:27 +0900

    skeleton json file instead of csv

--- scripts/visualization.py
+++ scripts/visualization.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# visualization.py: code for skeleton visualization
+# visualization.py: code for skeleton visualizatio.
+#                   skeleton info is taken from json file.
 # Author: Ravi Joshi
 # Date: 2017/12/21
+# Modified on: 2018/11/27
 
 # import modules
+import json
 import rospy
-import numpy as np
 from std_msgs.msg import ColorRGBA
 from geometry_msgs.msg import Vector3, Point
 from visualization_msgs.msg import Marker, MarkerArray
@@ -21,6 +23,8 @@ Hence, the skeleton is decomposed into three LINE_STRIP as following:
 
 See the link below to get the id of each joint as defined in Kinect v2
 source: https://msdn.microsoft.com/en-us/library/microsoft.kinect.jointtype.aspx
+new https://docs.microsoft.com/en-us/previous-versions/windows/kinect/dn758662%28v%3dieb.10%29
+
 
 upper_body:
 	head 3, neck 2, spine shoulder 20,
@@ -36,6 +40,48 @@ legs:
 	hip left 12, spine base 0, hip right 16
 	knee right 17, ankle right 18, foot right 19
 '''
+
+
+def read_skeleton_file(file_name):
+    # joints info is taken from kinect website as mentioned below
+    # https://docs.microsoft.com/en-us/previous-versions/windows/kinect/dn758662%28v%3dieb.10%29
+    joints_info = {'AnkleLeft': 14,
+                   'AnkleRight': 18,
+                   'ElbowLeft': 5,
+                   'ElbowRight': 9,
+                   'FootLeft': 15,
+                   'FootRight': 19,
+                   'HandLeft': 7,
+                   'HandRight': 11,
+                   'HandTipLeft': 21,
+                   'HandTipRight': 23,
+                   'Head': 3,
+                   'HipLeft': 12,
+                   'HipRight': 16,
+                   'KneeLeft': 13,
+                   'KneeRight': 17,
+                   'Neck': 2,
+                   'ShoulderLeft': 4,
+                   'ShoulderRight': 8,
+                   'SpineBase': 0,
+                   'SpineMid': 1,
+                   'SpineShoulder': 20,
+                   'ThumbLeft': 22,
+                   'ThumbRight': 24,
+                   'WristLeft': 6,
+                   'WristRight': 10}
+
+    # read json file as a dictionary
+    with open(file_name) as json_file:
+        joints = json.load(json_file)
+
+    # sort joints based on joint id
+    joint_list = [None] * len(joints_info)
+    for joint_name in joints:
+        joint_list[joints_info[joint_name]] = joints[joint_name]
+
+    return joint_list
+
 
 class Visualization():
     def __init__(self, ns, skeleton_frame, body_id_text_size, skeleton_line_width, file_name):
@@ -63,10 +109,10 @@ class Visualization():
 
         # for the demonstration, I am just using one person recorded data
         # however, it can be supplied here in real-time accquired using Kinect etc
-        skeleton_joints = np.loadtxt(file_name, delimiter=',', skiprows=1)
-        bodies = [skeleton_joints.tolist()]
+        skeleton_joints = read_skeleton_file(file_name)
+        bodies = [skeleton_joints]
 
-        rate = rospy.Rate(10) # 10hz
+        rate = rospy.Rate(10)  # 10hz
         while not rospy.is_shutdown():
             marker_index = 0
             person_index = 1
@@ -76,23 +122,59 @@ class Visualization():
                 now = rospy.Time.now()
 
                 marker_index += 1
-                upper_body = self.create_marker(marker_index, colors[person_index], Marker.LINE_STRIP, skeleton_line_width, now, skeleton_frame)
+                upper_body = self.create_marker(
+                    marker_index,
+                    colors[person_index],
+                    Marker.LINE_STRIP,
+                    skeleton_line_width,
+                    now,
+                    skeleton_frame)
 
                 marker_index += 1
-                hands = self.create_marker(marker_index, colors[person_index], Marker.LINE_STRIP, skeleton_line_width, now, skeleton_frame)
+                hands = self.create_marker(
+                    marker_index,
+                    colors[person_index],
+                    Marker.LINE_STRIP,
+                    skeleton_line_width,
+                    now,
+                    skeleton_frame)
 
                 marker_index += 1
-                legs = self.create_marker(marker_index, colors[person_index], Marker.LINE_STRIP, skeleton_line_width, now, skeleton_frame)
+                legs = self.create_marker(
+                    marker_index,
+                    colors[person_index],
+                    Marker.LINE_STRIP,
+                    skeleton_line_width,
+                    now,
+                    skeleton_frame)
 
                 all_joints = body
-                upper_body.points = [Point(all_joints[id][0], all_joints[id][1], all_joints[id][2]) for id in upper_body_ids]
-                hands.points = [Point(all_joints[id][0], all_joints[id][1], all_joints[id][2]) for id in hands_ids]
-                legs.points = [Point(all_joints[id][0], all_joints[id][1], all_joints[id][2]) for id in legs_ids]
+                upper_body.points = [Point(
+                    all_joints[id][0],
+                    all_joints[id][1],
+                    all_joints[id][2]) for id in upper_body_ids]
+                hands.points = [Point(
+                    all_joints[id][0],
+                    all_joints[id][1],
+                    all_joints[id][2]) for id in hands_ids]
+                legs.points = [Point(
+                    all_joints[id][0],
+                    all_joints[id][1],
+                    all_joints[id][2]) for id in legs_ids]
 
                 marker_index += 1
-                head_id_marker = self.create_marker(marker_index, body_id_color, Marker.TEXT_VIEW_FACING, body_id_text_size, now, skeleton_frame)
+                head_id_marker = self.create_marker(
+                    marker_index,
+                    body_id_color,
+                    Marker.TEXT_VIEW_FACING,
+                    body_id_text_size,
+                    now,
+                    skeleton_frame)
                 head_id_marker.text = str(person_index)
-                head_id_marker.pose.position = Point(all_joints[head_id][0], all_joints[head_id][1], all_joints[head_id][2])
+                head_id_marker.pose.position = Point(
+                    all_joints[head_id][0],
+                    all_joints[head_id][1],
+                    all_joints[head_id][2])
 
                 marker_array.markers.append(head_id_marker)
                 marker_array.markers.append(upper_body)
@@ -117,6 +199,7 @@ class Visualization():
         marker.lifetime = rospy.Duration(1)  # 1 second
         return marker
 
+
 if __name__ == '__main__':
     # define some constants
     ns = 'visualization'
@@ -126,4 +209,5 @@ if __name__ == '__main__':
 
     # initialize ros node
     rospy.init_node(ns, anonymous=True)
-    Visualization(ns, skeleton_frame, body_id_text_size, skeleton_line_width, rospy.get_param('~file'))
+    Visualization(ns, skeleton_frame, body_id_text_size,
+                  skeleton_line_width, rospy.get_param('~file'))
